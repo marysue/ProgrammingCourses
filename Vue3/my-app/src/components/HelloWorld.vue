f476897c0da18f79ab0831cb7439a2a45b09cbad
Mary Lark <mary@marylark.com>
2025-03-08 12:23:25 -0700

    Added new view dir and my-app

---
+++ Vue3/my-app/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Hello to my-app!</div>
+</template>
+
+<script setup>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
